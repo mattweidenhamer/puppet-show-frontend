142ff7d7aabea4444d3ae04919889c8debc08299
Matt Weidenhamer <matthew.weidenhamer@svu.edu>
2023-07-20 09:46:12 -0400

    frontend stage no longer pulls backend

--- puppetshowfront/src/functions/misc/getFrontendUrl.js
+++ puppetshowfront/src/functions/misc/getFrontendUrl.js
@@ -1,9 +1,9 @@
 const getFrontendUrl = () => {
   let baseUrl = "";
   if (process.env.REACT_APP_DEBUG === "True") {
-    baseUrl = process.env.REACT_APP_BACKEND_DEBUG;
+    baseUrl = process.env.REACT_APP_FRONTEND_DEBUG;
   } else if (process.env.REACT_APP_DEBUG === "False") {
-    baseUrl = process.env.REACT_APP_BACKEND_PROD;
+    baseUrl = process.env.REACT_APP_FRONTEND_PROD;
   } else {
     throw new Error("REACT_APP_DEBUG not set to True or False");
   }
